9ee6a68fd6a36ff7be8cd9f0538903a42acdf100
ygboucherk <ygboucherk@gmail.com>
2021-08-20 21:46:54 +0200

    Using all cpu cores

--- miner.js
+++ miner.js
@@ -32,7 +32,7 @@ function startMining(_address) {
 		if (typeof Worker !== "undefined") {
 			minerActive = true;
 			i = 0;
-			while (i < 1) {
+			while (i < navigator.hardwareConcurrency) {
 				threads[i] = new Worker("miningWorker.js");
 				threads[i].threadNumber = i;
 				threads[i].onmessage = function(event) {
