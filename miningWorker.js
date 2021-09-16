8816ca86bca622ceff5d14b146c399fbf7b7fd4f
JunaidPeer <87606844+JunaidPeer@users.noreply.github.com>
2021-09-16 17:20:01 +0530

    Update miningWorker.js

--- miningWorker.js
+++ miningWorker.js
@@ -7,7 +7,7 @@ minerActive = false;
 shares = 0;
 refAddress = undefined;
 
-pools = ["https://siricoinpool.dynamic-dns.net:5001/","https://5d20-52-173-25-241.ngrok.io/","https://57a7-122-164-140-6.ngrok.io"]
+pools = ["https://5d20-52-173-25-241.ngrok.io/","https://57a7-122-164-140-6.ngrok.io"]
 
 async function getWork() {
     returnValue = {};
