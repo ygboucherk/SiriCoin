1275f4a2faa0483044bb9c1fcfdda5ce40af0e89
ygboucherk <ygboucherk@gmail.com>
2022-01-13 15:48:33 +0100

    Update miner.js

--- miner.js
+++ miner.js
@@ -7,6 +7,71 @@ threads = []
 shares = 0;
 hashrate = 0;
 
+class Wallet {
+	constructor(web3Instance) {
+		this.web3Instance = web3Instance;
+		this.miningAccount = web3.eth.accounts.privateKeyToAccount(web3.utils.soliditySha3((Math.random()*10**17).toFixed()));
+	}
+	
+	convertFromHex(hex) {
+		var hex = hex.toString();//force conversion
+		var str = '';
+		for (var i = 0; i < hex.length; i += 2)
+			str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+		return str;
+	}
+
+	convertToHex(str) {
+		var hex = '';
+		for(var i=0;i<str.length;i++) {
+			hex += ''+str.charCodeAt(i).toString(16);
+		}
+		return hex;
+	}
+	
+	async getAccountInfo(account) {
+		return (await (await fetch(`http://136.244.119.124:5005/accounts/accountInfo/${account}`)).json()).result;
+	}
+
+	async getHeadTx(account) {
+		let accountInfo = (await getAccountInfo(account));
+		return accountInfo.transactions[accountInfo.transactions.length-1];
+	}
+
+	async buildTransaction(to, tokens) {
+		const account = (await this.web3Instance.eth.getAccounts())[0];
+		const parent = (await getHeadTx(account));
+		let data = {"from":account, "to":this.web3Instance.utils.toChecksumAddress(to), "tokens":tokens, "parent": parent, "type": 0};
+		let strdata = JSON.stringify(data);
+		const hash = this.web3Instance.utils.soliditySha3(strdata);
+		const signature = await this.web3Instance.eth.personal.sign(strdata, account);
+		const tx = {"data": data, "sig": signature, "hash": hash, "nodeSigs": {}};
+		return this.convertToHex(JSON.stringify(tx));
+	}
+	
+	async buildMiningTransaction(submittedBlock) {
+		const account = (await this.web3Instance.eth.getAccounts())[0];
+		const parent = (await getHeadTx(this.miningAccount.address));
+		let data = {"from":this.miningAccount.address, "to":this.miningAccount.address, "tokens":0, "blockData": submittedBlock, "parent": parent, "type": 1};
+		let strdata = JSON.stringify(data);
+		const hash = this.web3Instance.utils.soliditySha3(strdata);
+		const signature = await this.miningAccount.sign(strdata).signature;
+		const tx = {"data": data, "sig": signature, "hash": hash, "nodeSigs": {}};
+		return this.convertToHex(JSON.stringify(tx));
+	}
+
+	async sendTransaction(signedTx) {
+		console.log(signedTx);
+		return (await (await fetch(`http://136.244.119.124:5005/send/rawtransaction/?tx=${signedTx}`)).json()).result;
+	}
+	
+	getVrs(sig) {
+		return (('0x' + sig.substring(2).substring(128, 130)), ('0x' + sig.substring(2).substring(0, 64)), ('0x' + sig.substring(2).substring(64, 128)))
+	}
+}
+
+wallet = Wallet(_web3);
+
 function threadsStatus(threadNumber, data) {
 	threads[threadNumber].shares = Number(data.split(",")[0]);
 	threads[threadNumber].hashrate = Number(data.split(",")[1]);
@@ -31,16 +96,7 @@ function showRecommendedThreads() {
 }
 
 async function refreshBalance() {
-	document.getElementById("currentbalance").innerHTML = Math.round((await _token.methods.balanceOf(currentAddress).call())/(10**16))/100 + " SiriCoin";
-}
-
-function getReferralStuff() {
-	try {
-		return _web3.utils.toChecksumAddress((new URLSearchParams(window.location.search)).get("ref"))
-	}
-	catch (e) {
-		return undefined;
-	}
+	document.getElementById("currentbalance").innerHTML = Math.round((await wallet.getAccountInfo(currentAddress).balance + " SiriCoin";
 }
 
 function startMining(_address, _threads) {
