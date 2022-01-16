b2e2e08332ff2f0c1be3cb12a56aadc188e7009f
ygboucherk <ygboucherk@gmail.com>
2022-01-16 17:52:17 +0100

    Edit add to metamask

--- indexScripts.js
+++ indexScripts.js
@@ -1,6 +1,5 @@
 async function addToMetamask() {
 	if (typeof window.ethereum !== "undefined") {
-		await ethereum.request({ method: 'wallet_addEthereumChain', params: [{chainId: '0x89',chainName: 'Polygon',nativeCurrency:{name: 'Matic',symbol: 'Matic',decimals: 18},rpcUrls: ['https://rpc-mainnet.maticvigil.com/'],blockExplorerUrls :['https://polygonscan.com/'],}]})
-		await await ethereum.request({method: 'wallet_watchAsset',params: {type: 'ERC20',options: {address: "0x9eb1F7bc4875A09DfF51B1fb2f80f6B8E0b4EB81",symbol: "SC",decimals: 18,image: "https://sanjaysandy.github.io/SiriCoin/icons/logo.png",},},});
+		await ethereum.request({ method: 'wallet_addEthereumChain', params: [{chainId: '0x138d',chainName: 'SiriCoin',nativeCurrency:{name: 'SiriCoin',symbol: 'SC',decimals: 18},rpcUrls: ['https://siricoin-node-1.dynamic-dns.net/'],blockExplorerUrls :null,}]})
 	}
 }
